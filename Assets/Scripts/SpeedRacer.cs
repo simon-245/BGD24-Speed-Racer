71bacdb67bceba6e0760f40707f22151f0a43aa9
simon-245 <semjonfedun@gmail.com>
2024-11-20 23:56:20 +0200

    2nd part

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
-    string carModel = "GTR R35";
-    string engineType = "V6, Twin Turbo";
-    int carWeight = 1609;
-    int yearMade = 2009;
-    float maxAcceleration = 0.98f;
-    bool isCarTypeSedan = false;
-    bool hasFrontEngine = true;
+    public string carMaker;
+    public string carModel = "GTR R35";
+    public string engineType = "V6, Twin Turbo";
+    public int carWeight = 1609;
+    public int yearMade = 2009;
+    public float maxAcceleration = 0.98f;
+    public bool isCarTypeSedan = false;
+    public bool hasFrontEngine = true;
+
+    public class Fuel
+    {
+        public int fuelLevel;
+
+        public Fuel(int amount)
+        {
+            fuelLevel = amount;
+        }
+    }
+
+    public Fuel carFuel = new Fuel(100);
 
     void Start()
     {
-        print("The racer model is " + carModel + ". It has a " + engineType + " engine.");
+        print("The racer model is " + carModel + " by " + carMaker + ". It has a " + engineType + " engine.");
+
         CheckWeight();
 
         if (yearMade <= 2009)
@@ -28,11 +42,40 @@ public class SpeedRacer : MonoBehaviour
             print("It was first introduced in 2010s.");
             print("And its maximum acceleration is " + maxAcceleration + " m/s2");
         }
+        print(CheckCharacteristics());
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ConsumeFuel();
+            CheckFuelLevel();
+        }
+    }
 
+    void ConsumeFuel()
+    {
+        carFuel.fuelLevel = carFuel.fuelLevel - 10;
+    }
+
+    void CheckFuelLevel()
+    {
+        switch(carFuel.fuelLevel)
+        {
+            case 70:
+                print("Fuel level is nearing two-thirds.");
+                break;
+            case 50:
+                print("Fuel level is half amount.");
+                break;
+            case 10:
+                print("Warning! Fuel level is critically low.");
+                break;
+            default:
+                print("Nothing to report");
+                break;
+        }
     }
 
     void CheckWeight()
